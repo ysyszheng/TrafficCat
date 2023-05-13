2d7c029b9786710f284f5a56f39eae22dc7df5d5
Wang Xin <2364037658@qq.com>
2023-05-13 17:39:53 +0800

    add filter.h, define filter

---
+++ src/filter.h
@@ -0,0 +1,31 @@
+#ifndef FILTER_H
+#define FILTER_H
+
+#include "sniffer.h"
+#include "utils/utils.h"
+#include <QTableView>
+#include <map>
+#include <regex>
+#define P 0     /* protocal */
+#define S 1     /* source IP */
+#define D 2     /* dest IP */
+#define SPORT 3 /* source port */
+#define DPORT 4 /* dest port */
+#define C 5     /* packet content */
+
+class Filter : public QObject {
+  Q_OBJECT
+public:
+  Filter();
+  ~Filter();
+  bool checkCommand(QString command);
+  bool loadCommand(QString command);
+  void launchFilter(View *view);
+  bool launchOneFilter(const packet_struct *packet);
+
+private:
+  std::map<int, std::string> query;
+  std::string findWord(std::string command, size_t pos);
+};
+
+#endif
