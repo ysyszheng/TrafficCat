5d26e5dc4d1927c6c3f950425286225decd5ef11
Lai Jiang <future2020@sjtu.edu.cn>
2023-05-25 01:02:55 +0800

    devwindow.h

--- src/devwindow.h
+++ src/devwindow.h
@@ -4,20 +4,25 @@
 #include "./utils/utils.h"
 #include "sniffer.h"
 
+// Device selection window
 class DevWindow : public QDialog {
   Q_OBJECT
 public:
+// Constructor for the DevWindow class
   explicit DevWindow(Sniffer *snifferObj, QWidget *parent = nullptr);
   ~DevWindow();
 
 signals:
+// Signal for the sub window closed
   void subWndClosed();
 
 private slots:
+// Slot for the clicked signal
   void onClicked();
   void onToggled(bool checked);
 
 private:
+// Pointer to the Sniffer object
   Sniffer *sniffer;
   QGroupBox *devChoice;
   QRadioButton *selected;
